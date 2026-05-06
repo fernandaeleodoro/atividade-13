bc226f24b001fe7373af57b6cbd29d8bd8bf8476
fernandaeleodoro <fernanda_eleodoro@estudante.sesisenai.org.br>
2026-05-06 08:48:37 -0300

    1 2 3

--- scripr.js
+++ scripr.js
@@ -1,15 +1,4 @@
-const form = document.getElementById("formLogin");
-const mensagem = document.getElementById("mensagem");
-
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    const email = document.getElementById("email").value;
-    const senha = document.getElementById("senha").value;
-
-    if (email === "" || senha === "") {
-        mensagem.innerHTML = "<span style='color:red'>Preencha todos os campos!</span>";
-    } else {
-        mensagem.innerHTML = "<span style='color:green'>Login realizado com sucesso (simulado)</span>";
-    }
+document.getElementById("formLogin").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Login simulado!");
 });
